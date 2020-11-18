5b44c8729ec6fdb29f7776c3fd34b5f539beb368
Pugz <pugzfordayz132@gmail.com>
2020-11-17 22:10:45 -0500

    zombie horse transmutation

--- src/main/java/pugz/omni/core/module/MiscellaneousModule.java
+++ src/main/java/pugz/omni/core/module/MiscellaneousModule.java
@@ -1,17 +1,33 @@
 package pugz.omni.core.module;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ILivingEntityData;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.effect.LightningBoltEntity;
+import net.minecraft.entity.monster.ZombieEntity;
+import net.minecraft.entity.passive.horse.HorseEntity;
+import net.minecraft.entity.passive.horse.ZombieHorseEntity;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
 import net.minecraft.loot.LootTables;
 import net.minecraft.loot.TableLootEntry;
 import net.minecraft.loot.conditions.RandomChance;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.LootTableLoadEvent;
+import net.minecraftforge.event.entity.EntityStruckByLightningEvent;
 import pugz.omni.common.item.forestry.EnchantedGoldenCarrotItem;
 import pugz.omni.core.Omni;
 import pugz.omni.core.registry.OmniItems;
 import pugz.omni.core.util.RegistryUtil;
+
+import java.util.List;
 
 public class MiscellaneousModule extends AbstractModule {
     public static final MiscellaneousModule instance = new MiscellaneousModule();
@@ -28,6 +44,7 @@ public class MiscellaneousModule extends AbstractModule {
     @Override
     protected void onInitialize() {
         MinecraftForge.EVENT_BUS.addListener(this::onLootTableLoad);
+        MinecraftForge.EVENT_BUS.addListener(this::onEntityStruckByLightning);
     }
 
     @Override
@@ -129,6 +146,38 @@ public class MiscellaneousModule extends AbstractModule {
             if (name.toString().equals(LootTables.BASTION_TREASURE.toString())) chance *= 3.0F;
             LootPool pool = new LootPool.Builder().addEntry(TableLootEntry.builder(new ResourceLocation(Omni.MOD_ID, "injects/enchanted_golden_carrot"))).acceptCondition(RandomChance.builder(chance)).build();
             table.addPool(pool);
+        }
+    }
+
+    public void onEntityStruckByLightning(EntityStruckByLightningEvent event) {
+        LightningBoltEntity lightning = event.getLightning();
+        Entity entity = event.getEntity();
+        World world = lightning.getEntityWorld();
+
+        if (entity instanceof ZombieEntity || entity instanceof HorseEntity) {
+            List<Entity> list = world.getEntitiesInAABBexcluding(lightning, new AxisAlignedBB(lightning.getPosX() - 3.0D, lightning.getPosY() - 3.0D, lightning.getPosZ() - 3.0D, lightning.getPosX() + 3.0D, lightning.getPosY() + 6.0D + 3.0D, lightning.getPosZ() + 3.0D), Entity::isAlive);
+            list.forEach(target -> {
+                if (entity instanceof ZombieEntity && target instanceof HorseEntity) {
+                    transmutateZombieHorse(world, (ZombieEntity) entity, (HorseEntity) target);
+                } else if (entity instanceof HorseEntity && target instanceof ZombieEntity) {
+                    transmutateZombieHorse(world, (ZombieEntity) target, (HorseEntity) entity);
+                }
+            });
+        }
+    }
+
+    private void transmutateZombieHorse(World world, ZombieEntity zombie, HorseEntity horse) {
+        if (horse.hasCustomName() || zombie.hasCustomName() || horse.isTame()) return;
+        if (world instanceof ServerWorld) {
+            zombie.remove();
+            horse.remove();
+
+            Entity entity = EntityType.ZOMBIE_HORSE.spawn((ServerWorld) world, null, null, horse.getPosition(), SpawnReason.CONVERSION, false, false);
+            if (entity instanceof ZombieHorseEntity) {
+                ZombieHorseEntity zombieHorse = (ZombieHorseEntity)entity;
+                zombieHorse.setLocationAndAngles(entity.getPosX(), entity.getPosY(), entity.getPosZ(), entity.rotationYaw, 0.0F);
+                zombieHorse.onInitialSpawn((ServerWorld) world, new DifficultyInstance(world.getDifficulty(), world.getGameTime(), world.getChunkAt(entity.getPosition()).getInhabitedTime(), world.getMoonFactor()), SpawnReason.CONVERSION, (ILivingEntityData) null, (CompoundNBT) null);
+            }
         }
     }
 }
