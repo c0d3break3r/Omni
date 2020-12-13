06390a2cb90daabb299520a23aea34cd8636486d
Pugz <pugzfordayz132@gmail.com>
2020-12-12 19:02:06 -0500

    mixin fix
    -version bump

--- src/main/java/pugz/omni/core/mixin/CactusBlockMixin.java
+++ src/main/java/pugz/omni/core/mixin/CactusBlockMixin.java
@@ -2,6 +2,8 @@ package pugz.omni.core.mixin;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CactusBlock;
+import net.minecraft.block.material.Material;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorldReader;
@@ -13,10 +15,14 @@ import pugz.omni.core.registry.OmniBlocks;
 
 @Mixin(CactusBlock.class)
 public final class CactusBlockMixin {
-    @Inject(at = @At(value = "HEAD"), method = "isValidPosition")
+    @Inject(at = @At(value = "HEAD"), method = "isValidPosition", cancellable = true)
     private void isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos, CallbackInfoReturnable<Boolean> info) {
         for(Direction direction : Direction.Plane.HORIZONTAL) {
-            info.setReturnValue(worldIn.getBlockState(pos.offset(direction)).getBlock() != OmniBlocks.SAGUARO_CACTUS.get());
+            BlockState blockstate = worldIn.getBlockState(pos.offset(direction));
+            Material material = blockstate.getMaterial();
+            if (material.isSolid() || worldIn.getFluidState(pos.offset(direction)).isTagged(FluidTags.LAVA)) {
+                info.setReturnValue(blockstate.getBlock() != OmniBlocks.SAGUARO_CACTUS.get());
+            }
         }
     }
 }
