01eff688809c87f710b8fe9c07fd0920fc546a82
Pugz <pugzfordayz132@gmail.com>
2021-02-24 20:40:27 -0500

    fix leaves and atlas break temporary fix

--- src/main/java/pugz/omni/common/block/OmniLeavesBlock.java
+++ src/main/java/pugz/omni/common/block/OmniLeavesBlock.java
@@ -1,15 +1,10 @@
 package pugz.omni.common.block;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockReader;
 import pugz.omni.core.util.IBaseBlock;
 
-public class OmniLeavesBlock extends Block implements IBaseBlock {
+public class OmniLeavesBlock extends LeavesBlock implements IBaseBlock {
     public OmniLeavesBlock() {
         super(AbstractBlock.Properties.from(Blocks.OAK_LEAVES));
     }
@@ -27,14 +22,5 @@ public class OmniLeavesBlock extends Block implements IBaseBlock {
     @Override
     public RenderType getRenderType() {
         return RenderType.getCutoutMipped();
-    }
-
-    @SuppressWarnings("deprecation")
-    public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
-        return 1;
-    }
-
-    public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
-        return true;
     }
 }
